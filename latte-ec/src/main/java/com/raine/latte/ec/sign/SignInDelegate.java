c8d4bc5da99200c1ae9b7aec62d84afb1a41d856
zhengxiaobo <zhengxiaobo@jianbing.com>
2017-12-23 20:05:24 +0800

    update

---
+++ latte-ec/src/main/java/com/raine/latte/ec/sign/SignInDelegate.java
@@ -0,0 +1,25 @@
+package com.raine.latte.ec.sign;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.raine.latte.delegates.LatteDelegate;
+
+/**
+ * Created by Administrator on 2017/12/23.
+ */
+
+public class SignInDelegate extends LatteDelegate{
+
+
+    @Override
+    public Object setLayout() {
+        return null;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle saveInstanceState, View rootView) {
+
+    }
+}
